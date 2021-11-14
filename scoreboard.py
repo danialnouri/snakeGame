ff024871f70be2f5b533938b58cfd9390e8fdc47
danialNouri <danial.nouri20@gmail.com>
2021-11-14 22:15:19 +0330

    with highscore in text file...

--- scoreboard.py
+++ scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -22,8 +23,11 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
+
     # def game_over(self):
     #     self.goto(0, 0)
     #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
